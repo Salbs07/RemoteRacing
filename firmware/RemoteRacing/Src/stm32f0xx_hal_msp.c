91b58aee09444c139ae6ed70171d546f2533f662
Salbs07 <salbertd@umich.edu>
2020-11-02 01:01:43 -0500

    Initial PCB Layout

--- firmware/RemoteRacing/Src/stm32f0xx_hal_msp.c
+++ firmware/RemoteRacing/Src/stm32f0xx_hal_msp.c
@@ -77,6 +77,245 @@ void HAL_MspInit(void)
   /* USER CODE END MspInit 1 */
 }
 
+/**
+* @brief I2C MSP Initialization
+* This function configures the hardware resources used in this example
+* @param hi2c: I2C handle pointer
+* @retval None
+*/
+void HAL_I2C_MspInit(I2C_HandleTypeDef* hi2c)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(hi2c->Instance==I2C1)
+  {
+  /* USER CODE BEGIN I2C1_MspInit 0 */
+
+  /* USER CODE END I2C1_MspInit 0 */
+  
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    /**I2C1 GPIO Configuration    
+    PB7     ------> I2C1_SDA
+    PB8     ------> I2C1_SCL 
+    */
+    GPIO_InitStruct.Pin = IMU_SDA_Pin|IMU_SCL_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF1_I2C1;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /* Peripheral clock enable */
+    __HAL_RCC_I2C1_CLK_ENABLE();
+  /* USER CODE BEGIN I2C1_MspInit 1 */
+
+  /* USER CODE END I2C1_MspInit 1 */
+  }
+
+}
+
+/**
+* @brief I2C MSP De-Initialization
+* This function freeze the hardware resources used in this example
+* @param hi2c: I2C handle pointer
+* @retval None
+*/
+void HAL_I2C_MspDeInit(I2C_HandleTypeDef* hi2c)
+{
+  if(hi2c->Instance==I2C1)
+  {
+  /* USER CODE BEGIN I2C1_MspDeInit 0 */
+
+  /* USER CODE END I2C1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_I2C1_CLK_DISABLE();
+  
+    /**I2C1 GPIO Configuration    
+    PB7     ------> I2C1_SDA
+    PB8     ------> I2C1_SCL 
+    */
+    HAL_GPIO_DeInit(GPIOB, IMU_SDA_Pin|IMU_SCL_Pin);
+
+  /* USER CODE BEGIN I2C1_MspDeInit 1 */
+
+  /* USER CODE END I2C1_MspDeInit 1 */
+  }
+
+}
+
+/**
+* @brief SPI MSP Initialization
+* This function configures the hardware resources used in this example
+* @param hspi: SPI handle pointer
+* @retval None
+*/
+void HAL_SPI_MspInit(SPI_HandleTypeDef* hspi)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(hspi->Instance==SPI1)
+  {
+  /* USER CODE BEGIN SPI1_MspInit 0 */
+
+  /* USER CODE END SPI1_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_SPI1_CLK_ENABLE();
+  
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**SPI1 GPIO Configuration    
+    PA5     ------> SPI1_SCK
+    PA6     ------> SPI1_MISO
+    PA7     ------> SPI1_MOSI 
+    */
+    GPIO_InitStruct.Pin = LCD_SCK_Pin|LCD_MISO_Pin|LCD_MOSI_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF0_SPI1;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN SPI1_MspInit 1 */
+
+  /* USER CODE END SPI1_MspInit 1 */
+  }
+
+}
+
+/**
+* @brief SPI MSP De-Initialization
+* This function freeze the hardware resources used in this example
+* @param hspi: SPI handle pointer
+* @retval None
+*/
+void HAL_SPI_MspDeInit(SPI_HandleTypeDef* hspi)
+{
+  if(hspi->Instance==SPI1)
+  {
+  /* USER CODE BEGIN SPI1_MspDeInit 0 */
+
+  /* USER CODE END SPI1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_SPI1_CLK_DISABLE();
+  
+    /**SPI1 GPIO Configuration    
+    PA5     ------> SPI1_SCK
+    PA6     ------> SPI1_MISO
+    PA7     ------> SPI1_MOSI 
+    */
+    HAL_GPIO_DeInit(GPIOA, LCD_SCK_Pin|LCD_MISO_Pin|LCD_MOSI_Pin);
+
+  /* USER CODE BEGIN SPI1_MspDeInit 1 */
+
+  /* USER CODE END SPI1_MspDeInit 1 */
+  }
+
+}
+
+/**
+* @brief UART MSP Initialization
+* This function configures the hardware resources used in this example
+* @param huart: UART handle pointer
+* @retval None
+*/
+void HAL_UART_MspInit(UART_HandleTypeDef* huart)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(huart->Instance==USART1)
+  {
+  /* USER CODE BEGIN USART1_MspInit 0 */
+
+  /* USER CODE END USART1_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_USART1_CLK_ENABLE();
+  
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**USART1 GPIO Configuration    
+    PA9     ------> USART1_TX
+    PA10     ------> USART1_RX 
+    */
+    GPIO_InitStruct.Pin = BLE_RX_Pin|BLE_TX_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF1_USART1;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN USART1_MspInit 1 */
+
+  /* USER CODE END USART1_MspInit 1 */
+  }
+  else if(huart->Instance==USART2)
+  {
+  /* USER CODE BEGIN USART2_MspInit 0 */
+
+  /* USER CODE END USART2_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_USART2_CLK_ENABLE();
+  
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**USART2 GPIO Configuration    
+    PA2     ------> USART2_TX
+    PA3     ------> USART2_RX 
+    */
+    GPIO_InitStruct.Pin = GPS_RX_Pin|GPS_TX_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF1_USART2;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN USART2_MspInit 1 */
+
+  /* USER CODE END USART2_MspInit 1 */
+  }
+
+}
+
+/**
+* @brief UART MSP De-Initialization
+* This function freeze the hardware resources used in this example
+* @param huart: UART handle pointer
+* @retval None
+*/
+void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
+{
+  if(huart->Instance==USART1)
+  {
+  /* USER CODE BEGIN USART1_MspDeInit 0 */
+
+  /* USER CODE END USART1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_USART1_CLK_DISABLE();
+  
+    /**USART1 GPIO Configuration    
+    PA9     ------> USART1_TX
+    PA10     ------> USART1_RX 
+    */
+    HAL_GPIO_DeInit(GPIOA, BLE_RX_Pin|BLE_TX_Pin);
+
+  /* USER CODE BEGIN USART1_MspDeInit 1 */
+
+  /* USER CODE END USART1_MspDeInit 1 */
+  }
+  else if(huart->Instance==USART2)
+  {
+  /* USER CODE BEGIN USART2_MspDeInit 0 */
+
+  /* USER CODE END USART2_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_USART2_CLK_DISABLE();
+  
+    /**USART2 GPIO Configuration    
+    PA2     ------> USART2_TX
+    PA3     ------> USART2_RX 
+    */
+    HAL_GPIO_DeInit(GPIOA, GPS_RX_Pin|GPS_TX_Pin);
+
+  /* USER CODE BEGIN USART2_MspDeInit 1 */
+
+  /* USER CODE END USART2_MspDeInit 1 */
+  }
+
+}
+
 /* USER CODE BEGIN 1 */
 
 /* USER CODE END 1 */
---
+++ hardware/Misc.sch
@@ -0,0 +1,207 @@
+EESchema Schematic File Version 4
+LIBS:remote_racing_pcb-cache
+EELAYER 30 0
+EELAYER END
+$Descr A4 11693 8268
+encoding utf-8
+Sheet 2 7
+Title ""
+Date ""
+Rev ""
+Comp ""
+Comment1 ""
+Comment2 ""
+Comment3 ""
+Comment4 ""
+$EndDescr
+Text Notes 2800 3500 0    98   ~ 0
+Mounting Holes
+$Comp
+L Mechanical:MountingHole_Pad H?
+U 1 1 5F9B1D2A
+P 2650 3850
+AR Path="/5F9239BE/5F9B1D2A" Ref="H?"  Part="1" 
+AR Path="/5F96E2E5/5F9B1D2A" Ref="H1"  Part="1" 
+F 0 "H1" H 2750 3899 50  0000 L CNN
+F 1 "MountingHole_Pad" H 2750 3808 50  0000 L CNN
+F 2 "Mounting_Holes:MountingHole_4.3mm_M4" H 2650 3850 50  0001 C CNN
+F 3 "~" H 2650 3850 50  0001 C CNN
+	1    2650 3850
+	1    0    0    -1  
+$EndComp
+$Comp
+L Mechanical:MountingHole_Pad H?
+U 1 1 5F9B1D30
+P 3600 3850
+AR Path="/5F9239BE/5F9B1D30" Ref="H?"  Part="1" 
+AR Path="/5F96E2E5/5F9B1D30" Ref="H3"  Part="1" 
+F 0 "H3" H 3700 3899 50  0000 L CNN
+F 1 "MountingHole_Pad" H 3700 3808 50  0000 L CNN
+F 2 "Mounting_Holes:MountingHole_4.3mm_M4" H 3600 3850 50  0001 C CNN
+F 3 "~" H 3600 3850 50  0001 C CNN
+	1    3600 3850
+	1    0    0    -1  
+$EndComp
+$Comp
+L Mechanical:MountingHole_Pad H?
+U 1 1 5F9B1D36
+P 3600 4400
+AR Path="/5F9239BE/5F9B1D36" Ref="H?"  Part="1" 
+AR Path="/5F96E2E5/5F9B1D36" Ref="H4"  Part="1" 
+F 0 "H4" H 3700 4449 50  0000 L CNN
+F 1 "MountingHole_Pad" H 3700 4358 50  0000 L CNN
+F 2 "Mounting_Holes:MountingHole_4.3mm_M4" H 3600 4400 50  0001 C CNN
+F 3 "~" H 3600 4400 50  0001 C CNN
+	1    3600 4400
+	1    0    0    -1  
+$EndComp
+$Comp
+L Mechanical:MountingHole_Pad H?
+U 1 1 5F9B1D3C
+P 2650 4400
+AR Path="/5F9239BE/5F9B1D3C" Ref="H?"  Part="1" 
+AR Path="/5F96E2E5/5F9B1D3C" Ref="H2"  Part="1" 
+F 0 "H2" H 2750 4449 50  0000 L CNN
+F 1 "MountingHole_Pad" H 2750 4358 50  0000 L CNN
+F 2 "Mounting_Holes:MountingHole_4.3mm_M4" H 2650 4400 50  0001 C CNN
+F 3 "~" H 2650 4400 50  0001 C CNN
+	1    2650 4400
+	1    0    0    -1  
+$EndComp
+$Comp
+L power:GND #PWR?
+U 1 1 5F9B1D42
+P 2650 3950
+AR Path="/5F9239BE/5F9B1D42" Ref="#PWR?"  Part="1" 
+AR Path="/5F96E2E5/5F9B1D42" Ref="#PWR0101"  Part="1" 
+F 0 "#PWR0101" H 2650 3700 50  0001 C CNN
+F 1 "GND" H 2655 3777 50  0000 C CNN
+F 2 "" H 2650 3950 50  0001 C CNN
+F 3 "" H 2650 3950 50  0001 C CNN
+	1    2650 3950
+	1    0    0    -1  
+$EndComp
+$Comp
+L power:GND #PWR?
+U 1 1 5F9B1D48
+P 3600 3950
+AR Path="/5F9239BE/5F9B1D48" Ref="#PWR?"  Part="1" 
+AR Path="/5F96E2E5/5F9B1D48" Ref="#PWR0102"  Part="1" 
+F 0 "#PWR0102" H 3600 3700 50  0001 C CNN
+F 1 "GND" H 3605 3777 50  0000 C CNN
+F 2 "" H 3600 3950 50  0001 C CNN
+F 3 "" H 3600 3950 50  0001 C CNN
+	1    3600 3950
+	1    0    0    -1  
+$EndComp
+$Comp
+L power:GND #PWR?
+U 1 1 5F9B1D4E
+P 3600 4500
+AR Path="/5F9239BE/5F9B1D4E" Ref="#PWR?"  Part="1" 
+AR Path="/5F96E2E5/5F9B1D4E" Ref="#PWR0103"  Part="1" 
+F 0 "#PWR0103" H 3600 4250 50  0001 C CNN
+F 1 "GND" H 3605 4327 50  0000 C CNN
+F 2 "" H 3600 4500 50  0001 C CNN
+F 3 "" H 3600 4500 50  0001 C CNN
+	1    3600 4500
+	1    0    0    -1  
+$EndComp
+$Comp
+L power:GND #PWR?
+U 1 1 5F9B1D54
+P 2650 4500
+AR Path="/5F9239BE/5F9B1D54" Ref="#PWR?"  Part="1" 
+AR Path="/5F96E2E5/5F9B1D54" Ref="#PWR0104"  Part="1" 
+F 0 "#PWR0104" H 2650 4250 50  0001 C CNN
+F 1 "GND" H 2655 4327 50  0000 C CNN
+F 2 "" H 2650 4500 50  0001 C CNN
+F 3 "" H 2650 4500 50  0001 C CNN
+	1    2650 4500
+	1    0    0    -1  
+$EndComp
+Text Notes 7450 3200 0    98   ~ 0
+Test Points:
+$Comp
+L Connector:TestPoint TP8
+U 1 1 5F97FB23
+P 8150 4450
+F 0 "TP8" H 8208 4568 50  0001 L CNN
+F 1 "TestPoint" H 8208 4477 50  0000 L CNN
+F 2 "Testpoints:TestPoint_Keystone_5005-5009_Compact" H 8350 4450 50  0001 C CNN
+F 3 "https://www.mouser.com/datasheet/2/215/005-5009-743680.pdf" H 8350 4450 50  0001 C CNN
+	1    8150 4450
+	1    0    0    -1  
+$EndComp
+$Comp
+L Connector:TestPoint TP11
+U 1 1 5F97FB29
+P 8700 4450
+F 0 "TP11" H 8758 4568 50  0001 L CNN
+F 1 "TestPoint" H 8758 4477 50  0000 L CNN
+F 2 "Testpoints:TestPoint_Keystone_5005-5009_Compact" H 8900 4450 50  0001 C CNN
+F 3 "https://www.mouser.com/datasheet/2/215/005-5009-743680.pdf" H 8900 4450 50  0001 C CNN
+	1    8700 4450
+	1    0    0    -1  
+$EndComp
+$Comp
+L Connector:TestPoint TP13
+U 1 1 5F981BB8
+P 7550 3600
+F 0 "TP13" H 7608 3718 50  0001 L CNN
+F 1 "TestPoint" H 7608 3627 50  0000 L CNN
+F 2 "Testpoints:TestPoint_Keystone_5005-5009_Compact" H 7750 3600 50  0001 C CNN
+F 3 "https://www.mouser.com/datasheet/2/215/005-5009-743680.pdf" H 7750 3600 50  0001 C CNN
+	1    7550 3600
+	1    0    0    -1  
+$EndComp
+$Comp
+L Connector:TestPoint TP15
+U 1 1 5F981BBE
+P 8100 3600
+F 0 "TP15" H 8158 3718 50  0001 L CNN
+F 1 "TestPoint" H 8158 3627 50  0000 L CNN
+F 2 "Testpoints:TestPoint_Keystone_5005-5009_Compact" H 8300 3600 50  0001 C CNN
+F 3 "https://www.mouser.com/datasheet/2/215/005-5009-743680.pdf" H 8300 3600 50  0001 C CNN
+	1    8100 3600
+	1    0    0    -1  
+$EndComp
+Text GLabel 8100 3600 3    50   Input ~ 0
+IMU_SDA
+Text GLabel 7550 3600 3    50   Input ~ 0
+IMU_SCL
+Text Notes 7800 3400 0    50   ~ 0
+IMU
+Text GLabel 8150 4450 3    50   Input ~ 0
+GPS_1PPS
+Text GLabel 8700 4450 3    50   Input ~ 0
+GPS_FORCE_ON
+$Comp
+L Connector:TestPoint TP2
+U 1 1 5F9F48FE
+P 7000 4450
+F 0 "TP2" H 7058 4568 50  0001 L CNN
+F 1 "TestPoint" H 7058 4477 50  0000 L CNN
+F 2 "Testpoints:TestPoint_Keystone_5005-5009_Compact" H 7200 4450 50  0001 C CNN
+F 3 "https://www.mouser.com/datasheet/2/215/005-5009-743680.pdf" H 7200 4450 50  0001 C CNN
+	1    7000 4450
+	1    0    0    -1  
+$EndComp
+$Comp
+L Connector:TestPoint TP5
+U 1 1 5F9F4904
+P 7600 4450
+F 0 "TP5" H 7658 4568 50  0001 L CNN
+F 1 "TestPoint" H 7658 4477 50  0000 L CNN
+F 2 "Testpoints:TestPoint_Keystone_5005-5009_Compact" H 7800 4450 50  0001 C CNN
+F 3 "https://www.mouser.com/datasheet/2/215/005-5009-743680.pdf" H 7800 4450 50  0001 C CNN
+	1    7600 4450
+	1    0    0    -1  
+$EndComp
+Text GLabel 7000 4450 3    50   Input ~ 0
+GPS_TX
+Text Notes 7800 4250 0    50   ~ 0
+GPS
+Text GLabel 7600 4450 3    50   Input ~ 0
+GPS_RX
+$EndSCHEMATC
